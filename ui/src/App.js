c62bda5d19bc0b2ed173c4ed48502b6f21bfc6be
Zheenbek Akimzhanov <zheenbek.akimzhanov@cloudera.com>
2023-04-04 19:51:12 +0200

    UI: Basic Routes added

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,27 +1,48 @@
+import { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
 import "./App.css";
+import useLocalStorageState from "./util/useLocalStorageState";
+import Homepage from "./components/Homepage";
 
 function App() {
   console.log("Hello from App!");
 
-  const reqBody = {
-    email: "my_dummy_mail_1",
-    password: "my_dummy_password_1",
-  };
+  const [jwt, setJwt] = useLocalStorageState("", "jwt");
 
-  fetch("api/v1/auth/login", {
-    headers: {
-      "Content-Type": "application/json",
-    },
-    method: "POST",
-    body: JSON.stringify(reqBody),
-  })
-    .then(res => Promise.all([res.json(), res.headers]))
-    .then(([strBody, headers]) => {
-      console.log(strBody);
-      console.log(headers.get("authorization"));
-    });
+  useEffect(() => {
+    console.log("Hello from useEffect!");
 
-  return <div className="App">Hello World!</div>;
+    //if jwt already exists in the localStorage, then we do not need to re-fecth it
+    if (jwt) return;
+
+    const reqBody = {
+      email: "my_dummy_mail_1",
+      password: "my_dummy_password_1",
+    };
+
+    fetch("api/v1/auth/login", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(reqBody),
+    })
+      .then((res) => Promise.all([res.json(), res.headers]))
+      .then(([strBody, headers]) => {
+        setJwt(headers.get("authorization"));
+      });
+  });
+
+  return (
+    <Routes>
+      <Route exact path="/dashboard" element={<Dashboard />} />
+      <Route exact path="/" element={<Homepage />} />
+    </Routes>
+    // <div className="App">
+    //   <div>JWT value is: {jwt}</div>
+    // </div>
+  );
 }
 
 export default App;
---
+++ ui/src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import useLocalStorageState from "../util/useLocalStorageState";
+
+const Dashboard = () => {
+//   const [jwt, setJwt] = useLocalStorageState("", "jwt");
+
+    return (
+        <div>
+            Welcome to my dahsboard!
+        </div>
+    );
+};
+
+export default Dashboard;
