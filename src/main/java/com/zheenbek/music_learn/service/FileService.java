d0aee65c42bb40098c6382fd323ed904ebed4575
Zheenbek Akimzhanov <zheenbek.akimzhanov@gmail.com>
2023-12-04 05:03:20 +0600

    Fix media type

--- src/main/java/com/zheenbek/music_learn/service/FileService.java
+++ src/main/java/com/zheenbek/music_learn/service/FileService.java
@@ -40,6 +40,9 @@ public class FileService {
         //save in the system:
         File storedFile = serverFileStorageService.storeFile(file, file.getOriginalFilename());
         //save in the database:
+        System.out.println("File: " + file);
+        System.out.println("Media type: " + file.getContentType());
+        System.out.println("original name: " + file.getOriginalFilename());
         FileEntity fileEntity = fileEntityFromFile(storedFile, file.getContentType());
         return mapFileEntityToDto(fileRepository.save(fileEntity));
     }
@@ -55,7 +58,7 @@ public class FileService {
         if (fileEntity.getFilePath() != null && fileEntity.getFileName() != null) {
             fileDTO.setFilePath(FILES_SERVING_ENDPOINT + '/' + fileEntity.getFileName());
         }
-        fileDTO.setMediaType(fileDTO.getMediaType());
+        fileDTO.setMediaType(fileEntity.getMediaType());
         return fileDTO;
     }
 }
